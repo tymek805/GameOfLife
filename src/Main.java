13769104097714a50f3144bb86c716713c9fbed5
Filip-Kubecki <117228148+Filip-Kubecki@users.noreply.github.com>
2022-11-08 22:17:31 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        gameOfLife game = new gameOfLife();//Tworzenie obiektu klasy
+        
+        boolean[][] board = game.createEmpty();//Stworzenie tablicy samych Falsów
+        game.fillWithRand(board);//Wypełnienie losowo False i True
+        game.writeDown(board);//Wypisanie zawartości tablicy
+    }
 }
