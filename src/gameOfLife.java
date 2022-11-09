5f521c03d4b9b0fab33303feb5630d7f89a0034c
Filip-Kubecki <117228148+Filip-Kubecki@users.noreply.github.com>
2022-11-09 14:19:44 +0100

    Update gameOfLife.java

--- src/gameOfLife.java
+++ src/gameOfLife.java
@@ -1,8 +1,8 @@
 import java.util.Random;
 
 public class gameOfLife {
-    final public int x = 16; //height of board
-    final public int y = 16; //width of board
+    final public int x = 32; //height of board
+    final public int y = 64; //width of board
 
     public boolean[][] createEmpty(){
         boolean[][] myArray = new boolean[x][y];
@@ -15,12 +15,22 @@ public class gameOfLife {
     }
 
     public void writeDown(boolean[][] x){
+        for (int i = 0; i < x[1].length; i++) {
+            System.out.print("--");
+        }
+
         for (int i = 0; i < x.length; i++) {
+            System.out.print("|");
             for (int j = 0; j < x[i].length; j++) {
-                if (x[i][j]) System.out.print("X  ");
-                else System.out.print("0  ");
+                if (x[i][j]) System.out.print("■ ");
+                else System.out.print("  ");
             }
+            System.out.print("|");
             System.out.println();
+        }
+
+        for (int i = 0; i < x[1].length; i++) {
+            System.out.print("--");
         }
     }
 
@@ -32,5 +42,44 @@ public class gameOfLife {
             }
         }
     }
-}
 
+    public int howManyLive(boolean[][] x,int x1, int y1){
+        int suma = 0;
+        for (int k = -1; k < 2; k++) {
+            for (int i = -1; i < 2; i++) {
+                if (i != 0 || k != 0){
+                    if (x[x1+k][y1+i]){
+                        suma++;
+                    }
+                }
+            }
+        }
+        return suma;
+    }
+
+    public void startGame(boolean[][] x){
+        boolean[][] myArray = new boolean[x.length][x[0].length];
+        for (int i = 1; i < (x.length-2); i++) {
+            for (int j = 1; j < (x[i].length-2); j++) {
+                if (x[i][j]){
+                    if (howManyLive(x,i,j) < 4 && howManyLive(x,i,j) > 2) myArray[i][j] = true;
+                    else myArray[i][j] = false;
+                }else {
+                    if (howManyLive(x,i,j) == 3) myArray[i][j] = true;
+                    else myArray[i][j] = false;
+                }
+            }
+        }
+        changeArray(x,myArray);
+        writeDown(x);
+    }
+
+    public boolean[][] changeArray(boolean[][] x,boolean[][] y){
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x[i].length; j++) {
+                x[i][j] = y[i][j];
+            }
+        }
+        return x;
+    }
+}
