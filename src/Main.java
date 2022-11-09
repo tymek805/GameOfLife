9dc5f33f0f0094d8d4f0987ec87e39e215e77543
Filip-Kubecki <117228148+Filip-Kubecki@users.noreply.github.com>
2022-11-09 14:19:16 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         gameOfLife game = new gameOfLife();//Tworzenie obiektu klasy
-        
+
         boolean[][] board = game.createEmpty();//Stworzenie tablicy samych Falsów
         game.fillWithRand(board);//Wypełnienie losowo False i True
         game.writeDown(board);//Wypisanie zawartości tablicy
+        System.out.println();
+
+        while (true){
+            game.startGame(board);
+            System.out.println();
+            Thread.sleep(1000);
+        }
     }
 }
