4cc149c52fc220b252e2c7efd91f0169c8ddbfa0
Tymoteusz <tymoteusz.lango@gmail.com>
2022-11-14 16:40:28 +0100

    Initial GUI v2

--- src/GUI.java
+++ src/GUI.java
@@ -4,31 +4,19 @@ import javax.swing.*;
 
 public class GUI
 {
-    JFrame frameObj;
+    JFrame boardGUI;
 
     GUI()
     {
-        frameObj = new JFrame();
+        boardGUI = new JFrame();
 
-        // creating 9 buttons
-        JButton btn1 = new JButton("1");
-        JButton btn2 = new JButton("2");
-        JButton btn3 = new JButton("3");
-        JButton btn4 = new JButton("4");
-        JButton btn5 = new JButton("5");
-        JButton btn6 = new JButton("6");
-        JButton btn7 = new JButton("7");
-        JButton btn8 = new JButton("8");
-        JButton btn9 = new JButton("9");
+        for (int i = 0; i < 400; i++) {
+            boardGUI.add(new JButton());
+        }
 
-        frameObj.add(btn1); frameObj.add(btn2); frameObj.add(btn3);
-        frameObj.add(btn4); frameObj.add(btn5); frameObj.add(btn6);
-        frameObj.add(btn7); frameObj.add(btn8); frameObj.add(btn9);
-
-
-        frameObj.setLayout(new GridLayout());
-        frameObj.setSize(300, 300);
-        frameObj.setVisible(true); // Zawsze musi być true
+        boardGUI.setLayout(new GridLayout(20, 20));
+        boardGUI.setSize(500, 500);
+        boardGUI.setVisible(true); // Zawsze musi być true
     }
 
     public static void main(String argvs[])
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        gameOfLife game = new gameOfLife(10, 10); // Tworzenie obiektu klasy
+        gameOfLife game = new gameOfLife(32, 32); // Tworzenie obiektu klasy
         boolean isnotEmpty = true;
         while (isnotEmpty){
             game.startGame();
