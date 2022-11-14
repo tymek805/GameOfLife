bc7e6a2c0697bc0b96b6ed0c50746de3acd785a5
Versim <wojciechkrzos67@gmail.com>
2022-11-14 16:07:41 +0100

    Console clearing

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        gameOfLife game = new gameOfLife(10, 10); // Tworzenie obiektu klasy
+    public static void main(String[] args) throws InterruptedException, IOException {
+        gameOfLife game = new gameOfLife(32, 32); // Tworzenie obiektu klasy
         boolean isnotEmpty = true;
         while (isnotEmpty){
             game.startGame();
             System.out.println();
             isnotEmpty = game.is_not_Empty();
-            Thread.sleep(1000);
+            Thread.sleep(50);
+            game.clearConsole();
         }
     }
 }
